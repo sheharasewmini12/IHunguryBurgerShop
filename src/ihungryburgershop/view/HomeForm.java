21fb4b72c395b5fb676340f5b6f06e0f32b19bb9
sheharasewmini12 <shehara.sewmini.ul@gmail.com>
2026-08-21 21:27:53 +0530

    changes

--- src/ihungryburgershop/view/HomeForm.java
+++ src/ihungryburgershop/view/HomeForm.java
@@ -122,7 +122,9 @@ public class HomeForm extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        BestCustomerForm bestCustomerForm = new BestCustomerForm();
+        bestCustomerForm.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
@@ -132,7 +134,9 @@ public class HomeForm extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
+        ViewOrdersForm viewOrdersForm = new ViewOrdersForm();
+        viewOrdersForm.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
